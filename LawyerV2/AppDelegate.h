7fad8479c2fa6a9c95b19c010aba7916c9dfc523
aevans002 <allanevans2005@msn.com>
2015-07-05 12:22:41 -0600

    Initial Commit

---
+++ LawyerV2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LawyerV2
+//
+//  Created by Administrator on 7/5/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
